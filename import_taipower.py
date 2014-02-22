e1b2d5bc9c56d9a8b5812fa2787f066d6a32b63e
Jeff Hung <jeff.cc.hung@gmail.com>
2014-02-22 15:25:12 +0800

    Fix: t should be unix_time.

--- import_taipower.py
+++ import_taipower.py
@@ -3,6 +3,12 @@ import sys
 import datetime
 import sqlite3
 
+# http://stackoverflow.com/questions/6999726/python-getting-millis-since-epoch-from-datetime/11111177#11111177
+def unix_time(dt):
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    delta = dt - epoch
+    return delta.total_seconds()
+
 def import_taipower(db_path, csv_path):
     with sqlite3.connect(db_path) as conn:
         conn.text_factory = str
@@ -29,7 +35,7 @@ CREATE TABLE taipower
                 generating  = row[4]
 
                 conn.execute('INSERT INTO taipower VALUES (?, ?, ?, ?, ?)',
-                             [timestamp, planet, powerset, generating, capacity])
+                             [unix_time(timestamp), planet, powerset, generating, capacity])
                 conn.commit()
                 sys.stderr.write('.')
 
@@ -37,6 +43,6 @@ if __name__ == '__main__':
     if len(sys.argv) == 3:
         import_taipower(sys.argv[1], sys.argv[2])
     else:
-        print 'Usage: %s <csv-file> <db-file>' % (sys.argv[0])
+        print 'Usage: %s <db-file> <csv-file>' % (sys.argv[0])
 
 
