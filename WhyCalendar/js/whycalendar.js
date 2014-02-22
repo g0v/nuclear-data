f16c92ea6b5efd81da61c209f4ee117d2367b4f4
junsuwhy <junsuwhy@netivism.com.tw>
2014-02-22 15:42:47 +0800

    Change position of first day in every month.

--- WhyCalendar/js/whycalendar.js
+++ WhyCalendar/js/whycalendar.js
@@ -1,9 +1,10 @@
+var arrDay = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
 var nextday = function (today) {
     return new Date(today.getTime() + 86400000);
 }
 
 var getweekday = function (day) {
-    var arrDay = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     return arrDay[day.getDay()];
 }
 
@@ -11,21 +12,29 @@ var add_cal = function (today, $cal) {
     var month = today.getMonth() + 1;
     var day = today.getDate();
     var weekday = getweekday(today);
-    $('<div class="day day-' + day + ' week-' + weekday + '">').html('<p>' + day + '</p>').appendTo($choose_month($cal, month));
+    if (day == 1) {
+        for (var i = 0; i < today.getDay(); i++) {
+            $choose_month($cal, month).append($('<div class="day none">'));
+        }
+
+    }
+    $('<div class="day day-' + day + ' week-' + weekday + '">').html('<p>' + day + '</p>').attr('data-date', today.getFullYear() + "-" + month + "-" + day).appendTo($choose_month($cal, month));
 
 }
 
 //如果沒有月份，插入月份的div
 $choose_month = function ($cal, month) {
-    if ($(cal).children('.month-' + month).length == 0)
-        return $('<div class="month month-' + month + '">').html('<h2>' + month + '月</h2>').appendTo($(cal));
-    else return $(cal).children('.month-' + month)
+    if ($(cal).children('.month-' + month).length == 0) {
+        var $month = $('<div class="month month-' + month + '">').html('<h2>' + month + '月</h2>').appendTo($(cal));
+        return $month;
+    } else return $(cal).children('.month-' + month)
 
 }
 
+
 $(function () {
     var today = new Date("2013-01-01");
-    $cal = $('<div id="cal"></div>').appendTo('.wrapper');
+    $cal = $('<div id="cal"></div>').appendTo('.wrapper').append($('<h1>2013年</h1>'));
     // console.log(choose_month($cal, 1).text('1234'));
     for (var i = 0; i < 365; i++) {
 
