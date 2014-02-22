386953c7ad2e7a89ee3f587447244e34ddb70b76
Jeff Hung <jeff.cc.hung@gmail.com>
2014-02-22 22:04:44 +0800

    FIX: timedelta.total_seconds() is new in py26.
    And the default python version in some FreeBSD boxes is Python 2.6.

--- nuclear_utils.py
+++ nuclear_utils.py
@@ -1,8 +1,15 @@
+import sys
 import datetime
+
+def _timedelta_total_seconds(td):
+	if sys.version_info < (2, 7):
+		return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6
+	else:
+		return td.total_seconds()
 
 # http://stackoverflow.com/questions/6999726/python-getting-millis-since-epoch-from-datetime/11111177#11111177
 def unix_time(dt):
     epoch = datetime.datetime.utcfromtimestamp(0)
     delta = dt - epoch
-    return int(delta.total_seconds())
+    return int(_timedelta_total_seconds(delta))
 
